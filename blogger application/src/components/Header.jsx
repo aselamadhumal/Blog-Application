e374c34bf9c7a0f0d4dfb26b2e687730091aab1d
aselamadhumal <madhumalkumara2021@gmail.com>
2024-03-08 09:10:55 +0530

    cHANGE5

--- blogger application/src/components/Header.jsx
+++ blogger application/src/components/Header.jsx
@@ -1,5 +1,6 @@
-import { Navbar } from "flowbite-react";
+import { Button, Navbar, TextInput } from "flowbite-react";
 import { Link } from "react-router-dom";
+import { AiOutlineSearch} from 'react-icons/ai';
 
 export default function Header() {
     return (
@@ -9,6 +10,53 @@ export default function Header() {
           Asela's Blog
         </span>
         </Link>
-        </Navbar>
-    )
+
+        <form>
+        <TextInput put
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline'
+          
+        />
+      </form>
+      <Button className ='w-12 h-10' color='gray' pill>
+        <AiOutlineSearch />
+      </Button>
+
+    <div className="">
+    <Button className='w-12 h-10 hidden sm:inline' color="gray" pill>
+        
+        </Button>
+
+        <Link to='/SignIn'>
+
+            <Button gradientDuoTone = 'purplrToBlue' outline>
+
+                Sign In
+
+            </Button>
+
+        </Link>
+        <Navbar.Collapse>
+            <Navbar.Link>
+                <Link to = '/'>
+                    Home
+                </Link>
+            </Navbar.Link>
+            <Navbar.Link>
+                <Link to = '/about'>
+                About
+                </Link>
+            </Navbar.Link>
+
+            <Navbar.Link>
+                <Link to = '/Projects'>
+                Projects
+                </Link>
+            </Navbar.Link>
+        </Navbar.Collapse>
+    </div>
+        </Navbar> 
+    );
   }
