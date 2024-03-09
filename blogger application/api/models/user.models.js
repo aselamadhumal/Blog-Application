724e20db97ee57d90ca030d695bf4628f35ab96d
aselamadhumal <madhumalkumara2021@gmail.com>
2024-03-09 12:00:41 +0530

    Create User Model

---
+++ blogger application/api/models/user.models.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+
+    },
+},{timestamps:true}//to save extra information
+
+
+);
+
+//creating the model
+const User = mongoose.model('User',userSchema);
+
+export default User;
