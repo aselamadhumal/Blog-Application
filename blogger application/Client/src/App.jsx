2afa4b185a5980d270146f5ae1edde5e6e744cd1
aselamadhumal <madhumalkumara2021@gmail.com>
2024-03-25 16:55:15 +0530

    create the footer component

--- blogger application/Client/src/App.jsx
+++ blogger application/Client/src/App.jsx
@@ -7,6 +7,7 @@ import Projects from './pages/Projects'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 
@@ -21,7 +22,9 @@ export default function App() {
     <Route path="/Projects" element = {<Projects />} />
     <Route path="/SignIn" element = {<SignIn />} />
     <Route path="/SignUp" element = {<SignUp />} />
-   </Routes></BrowserRouter>
+   </Routes>
+   <Footer />
+   </BrowserRouter>
    
   )
 }
